d7c90b26cfc5bed9bcaaa606ac8d7088eb2ad2a0
schuasta <fabian.schustereder@gmail.com>
2014-03-06 14:56:19 +0100

    Revert "Sudoku"
    This reverts commit 1e87c6714e14d67ce9b05b74f7d7e31b0797eb6a.

--- SudokuProjekt/gen/at/fschustereder/android/sudokuprojekt/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package at.fschustereder.android.sudokuprojekt;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
