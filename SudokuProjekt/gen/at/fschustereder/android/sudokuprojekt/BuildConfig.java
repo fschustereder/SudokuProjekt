1e87c6714e14d67ce9b05b74f7d7e31b0797eb6a
schuasta <fabian.schustereder@gmail.com>
2014-03-06 14:54:46 +0100

    Sudoku
    Des is unsa projekt

---
+++ SudokuProjekt/gen/at/fschustereder/android/sudokuprojekt/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package at.fschustereder.android.sudokuprojekt;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
